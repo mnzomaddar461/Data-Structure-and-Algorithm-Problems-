29e0b0d45d1bced1188b7b89177e36b0db6d767c
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2025-12-24 09:41:59 +0600

    Linked list v

---
+++ Week 02/Linkued list v.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int value;
+    Node* next;
+    Node(int value){
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insart_value_in_tail(Node* &head, int value){
+    Node* newnode = new Node(value);
+        Node* temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+    temp->next = newnode;
+}
+
+void print_linked_list(Node* head){
+    Node* temp = head;
+        while(temp != NULL){
+        cout << temp->value << endl;
+        temp = temp->next;
+        }
+}
+int main(){
+    Node* head = new Node(10);
+    Node* node1 = new Node(20);
+    Node* node2 = new Node(30);
+    Node* node3 = new Node(30);
+
+    head->next = node1;
+    node1->next = node2;
+    node2->next = node3;
+
+    insart_value_in_tail(head, 50);
+    print_linked_list(head);
+
+    return 0;
+}
