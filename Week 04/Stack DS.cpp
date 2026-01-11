6f489b0023bf467cee52ea22d7e0de3933b9c9e9
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-01-11 21:34:06 +0600

    Stack DS

---
+++ Week 04/Stack DS.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class myStack{
+    public:
+    vector<int> stack;
+
+    void push(int value){
+        stack.push_back(value);
+    }
+
+    void pop(){
+        stack.pop_back();
+    }
+
+    int top(){
+        return stack.back();
+    }
+
+    int size(){
+        return stack.size();
+    }
+
+    bool empty(){
+        return stack.empty();
+    }
+
+};
+int main(){
+    myStack st;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while(!st.empty()){
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    return 0;
+}
