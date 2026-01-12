545cda446b038d43e22a33e59ff1aff5a660b3e7
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-01-12 20:07:42 +0600

    Queue DS

---
+++ Week 04/Queue DS using list.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class myQueue{
+    public:
+        list<int> li;
+
+        void push(int value){
+            li.push_back(value);
+        }
+
+        void pop(){
+            li.pop_front();
+        }
+
+        int font(){
+            return li.front();
+        }
+
+        int back(){
+            return li.back();
+        }
+
+        int size(){
+            return li.size();
+        }
+
+        bool empty(){
+            return li.empty();
+        }
+};
+int main(){
+    myQueue qe;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        qe.push(x);
+    }
+
+    cout << "Current Font, Back & Size: " << qe.font() << " , " << qe.back() << " & " << qe.size() << endl;
+
+    while(!qe.empty()){
+        cout << qe.font() << endl;
+        qe.pop();
+    }
+    return 0;
+}
