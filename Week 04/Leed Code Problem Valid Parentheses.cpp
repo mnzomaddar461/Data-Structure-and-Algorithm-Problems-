adb5a7a4862bbc6ff9b7fa6b210f4b18a962a6e3
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-01-15 23:11:40 +0600

    Leet Code Problem

---
+++ Week 04/Leed Code Problem Valid Parentheses.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+class ValidParentheses{
+    public:
+        bool isValid(string s){
+            stack<char> st;
+            for(char c : s){
+                if(c == '(' || c == '{' || c == '['){
+                    st.push(c);
+                }
+                else{
+                    if(st.empty()){
+                        return false;
+                    }
+                    else{
+                        if(c == ')' && st.top() == '('){
+                            st.pop();
+                        }
+                        else if(c == '}' && st.top() == '{'){
+                            st.pop();
+                        }
+                        else if(c == ']' && st.top() == '['){
+                            st.pop();
+                        }
+                        else return false;
+                    }
+                }
+            }
+            if(st.empty()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+};
+int main(){
+    string s;
+    cin >> s;
+
+    ValidParentheses vp;          
+    bool result = vp.isValid(s);  
+
+    if(result)
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
