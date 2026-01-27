e1a1f58a1aa57c490d404a6cba1bc7fbaa272066
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-01-28 03:40:00 +0600

    Tree DS

---
+++ Week 05/Define max height in binary tree.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* left;
+    Node* right;
+    Node(int value){
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* inputTree(){
+    int val;
+    cin >> val;
+    Node* root;
+    if(val == -1){
+        root = NULL;
+    }
+    else{
+        root = new Node(val);
+    }
+    queue<Node*> q;
+    if(root != NULL){
+        q.push(root);
+    }
+
+    while (!q.empty())
+    {
+        Node* prent = q.front();
+        q.pop();
+        int leftval, rightval;
+        cin >> leftval >> rightval;
+        Node* newleft, *newright;
+        if(leftval == -1){
+            newleft = NULL;
+        }
+        else{
+            newleft = new Node(leftval);
+        }
+        if(rightval == -1){
+            newright = NULL;
+        }else{
+            newright = new Node(rightval);
+        }
+
+        prent->left = newleft;
+        prent->right = newright;
+
+        if(prent->left != NULL){
+            q.push(prent->left);
+        }
+        if(prent->right != NULL){
+            q.push(prent->right);
+        }
+    }
+    return root;
+}
+
+int max_height(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    if(root->left == NULL && root->right == NULL){
+        return 0;
+    }
+    int l = max_height(root->left);
+    int r = max_height(root->right);
+    return max(l,r) + 1;
+}
+int main(){
+    Node* root = inputTree();
+    cout << max_height(root) << endl;
+    return 0;
+}
